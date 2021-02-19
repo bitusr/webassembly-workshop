49481e983cab64a268eda692987fc337d4872852
bitusr <dm.kolotov@mail.ru>
2021-02-19 22:47:38 +0100

    Add comment on express usage

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+// don't do in production!!!
 app.use(express.static('./'))
 
 app.listen(3000, () => console.log('Server is running on port 3000'))
