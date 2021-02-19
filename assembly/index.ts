347e08e568be5e139883247cbdf6377de119401e
bitusr <dm.kolotov@mail.ru>
2021-02-20 00:01:37 +0100

    Add performance testing - wasm vs js

--- assembly/index.ts
+++ assembly/index.ts
@@ -25,3 +25,17 @@ store<u8>(1, 99)
 export function readMemory(n: i32): i32 {
   return load<u8>(n) // load from memory
 }
+
+export function isPrimeWasm(x: u32): bool {
+  if (x < 2) {
+    return false;
+  }
+
+  for (let i: u32 = 2; i < x; i++) {
+    if (x % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
